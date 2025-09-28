0cceff75c056916efa91206091aba2fc990be5f5
xxxZino <egasaputraaa12@students.amikom.ac.id>
2025-09-28 22:33:14 +0700

    update project

--- src/projects.ts
+++ src/projects.ts
@@ -3,11 +3,36 @@ import type { Project } from "./types";
 
 export const projects: Project[] = [
   {
+    title: "Client Work – Personal Portfolio ",
+    desc: "A modern portfolio website built with Next.js and TailwindCSS, designed to look smooth, elegant, and full of clean animations.",
+    tech: ["Next js", "TailwindCSS", "Type Script", "React"],
+    img: "/portrangga.png",
+    demo: "https://rangga-portfolio-sigma.vercel.app/",
+    repo: "https://github.com/xxxZino/Job-Recommender",
+  },
+  {
+    title: "Client Work – Personal Portfolio",
+    desc: "A responsive portfolio website powered by Next.js and TailwindCSS with a focus on modern design and performance.",
+    tech: ["Next js", "TailwindCSS", "Type Script", "React"],
+    img: "/Preview.png",
+    demo: "https://sj-portofolio.vercel.app/",
+    repo: "https://github.com/xxxZino/Job-Recommender",
+  },
+    {
+    title: "Client Work – Company Profile",
+    desc: "A modern company profile system built with Next.js and TailwindCSS, designed to be dynamic and reliable.",
+    tech: ["Next js", "TailwindCSS", "Type Script", "React"],
+    img: "/jambuku.png",
+    demo: "https://jambuku.vercel.app/",
+    repo: "https://github.com/xxxZino/Job-Recommender",
+  },
+
+  {
     title: "Job Candidate Recommender",
-    desc: "NLP system to match candidates with vacancies using Sentence-BERT + Cosine Similarity.",
-    tech: ["Python", "Streamlit", "SBERT", "Pandas"],
-    img: "/recsystem.png",
+    desc: "NLP system to match candidates with vacancies using Sentence-BERT and Cosine similarity.",
+    tech: ["Python", "Streamlit", "SBERT", "Cosine Similarity"],
+    img: "/recsystem1.png",
     demo: "#",
-    repo: "#",
+    repo: "https://github.com/xxxZino/Job-Recommender",
   },
 ];
